0ccef1a0d3aef0ddae4f8b23d72926e2fd7e6309
jomariiiii <Jomari_b7@Yahoo.com>
2025-05-06 08:37:55 +0800

    Initial Commit
    first commit of the system

---
+++ studentinfosystem/StudentInfoSystem/src/main/java/group1/studentinfosystem/StudentInfoSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package group1.studentinfosystem;
+
+/**
+ *
+ * @author joshu
+ */
+public class StudentInfoSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
